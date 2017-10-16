c5177000bd73d38435990d588d1eab75c4aee267
Yuki <yuki.mul.tiplus@gmail.com>
2017-10-17 07:43:38 +0900

    remode ()

--- src/test/scala/esa/client/EsaClientSpec.scala
+++ src/test/scala/esa/client/EsaClientSpec.scala
@@ -33,19 +33,19 @@ class EsaClientSpec extends WordSpec with Matchers {
     "sendPost" in {
       val response = esaClient.sendPost("/v1/teams", Map())
       response.status should equal(404)
-      response.headers should not equal(empty)
+      response.headers should not equal empty
     }
 
     "sendPut" in {
       val response = esaClient.sendPut("/v1/teams", Map())
       response.status should equal(404)
-      response.headers should not equal(empty)
+      response.headers should not equal empty
     }
 
     "sendDelete" in {
       val response = esaClient.sendDelete("/v1/teams", Map())
       response.status should equal(404)
-      response.headers should not equal(empty)
+      response.headers should not equal empty
     }
   }
 }
